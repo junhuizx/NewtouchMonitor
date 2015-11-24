68cb9d581eece1c385fcd519501dd9bfe6ef101b
zouyapeng <zyp19901009@163.com>
2015-11-24 10:35:40 +0800

    some fixed

--- dashboard/hypervisors/urls.py
+++ dashboard/hypervisors/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from views import IndexView
+from views import IndexView,MonitorView,MonitorDetailView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view() ,name='index'),
+    url(r'^overview/$', IndexView.as_view() ,name='overview'),
+    url(r'^monitor/$', MonitorView.as_view() ,name='monitor'),
+    url(r'^monitor/<?P(pk)\d+>/$', MonitorDetailView.as_view() ,name='hypervisor_detail'),
 ]
--- dashboard/hypervisors/views.py
+++ dashboard/hypervisors/views.py
@@ -4,3 +4,11 @@ from models import *
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = 'hypervisors/index.html'
+
+class MonitorView(generic.ListView):
+    template_name = 'hypervisors/monitor.html'
+    queryset = []
+
+class MonitorDetailView(generic.DetailView):
+    template_name = 'hypervisors/monitor_detail.html'
+
